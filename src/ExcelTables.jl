20c8f6798bbf786414dbf1b3b201dc2d0dc4d18a
Min-Woong Sohn <mwsohn@gmail.com>
2017-12-27 17:37:36 -0500

    Initial commit

--- src/ExcelTables.jl
+++ src/ExcelTables.jl
@@ -1,5 +1,33 @@
+VERSION >= v"0.6.0" && __precompile__()
+
 module ExcelTables
 
-# package code goes here
+################################################################################
+##
+## Dependencies
+##
+################################################################################
+
+using DataFrames, PyCall, Distributions, GLM, StatsBase, DataStructures, HypothesisTests, NamedArrays, FreqTables
+
+##############################################################################
+##
+## Exported methods and types (in addition to everything reexported above)
+##
+##############################################################################
+
+export  univariatexls, # output univariate statistics in an excel worksheet
+        bivariatexls,  # output bivariate statistics in an excel worksheet
+        glmxls,        # output GLM models to an excel worksheet
+        mglmxls,       # output multiple GLM regression models to an excel spreadsheet
+        dfxls          # output dataframe in an excel file
+
+##############################################################################
+##
+## Load files
+##
+##############################################################################
+include("xlsout.jl")
+include("mglmxls.jl")
 
 end # module
