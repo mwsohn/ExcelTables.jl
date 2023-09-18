994f09fe1732a34bed788e5730127d638601d4b0
mwsohn <mwsohn@gmail.com>
2023-09-18 13:56:57 -0400

    Update xlsout.jl

--- src/xlsout.jl
+++ src/xlsout.jl
@@ -634,7 +634,7 @@ function bivariatexls(df::DataFrame,
                 end
             end
             if size(y,1) > 1
-                pval = anova(df3,varname,colvar).p[1]
+                pval = Stella.anova(df3,varname,colvar; table=true)[1,6]
                 if isnan(pval) || isinf(pval)
                     pval = ""
                 elseif pval < 0.001
