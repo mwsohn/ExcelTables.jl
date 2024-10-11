9a5fbe2940c8d1d2c26ad9f3cb3b8221925a34f5
mwsohn <mwsohn@gmail.com>
2024-10-10 22:52:51 -0400

    Update mglmxls.jl

--- src/mglmxls.jl
+++ src/mglmxls.jl
@@ -386,7 +386,7 @@ function mglmxls(glmout,
 
                 # ROC (c-statistic)
                 t.write(r+4,c,"Area under the ROC Curve",formats[:model_name])
-                _roc = AUC(roc(predict(glmout[i]), glmout[i].model.rr.y .== 1., true))
+                _roc = AUC(roc(predict(glmout[i]), glmout[i].model.rr.y, true))
                 t.merge_range(r+4,c+1,r+4,c+4,round(_roc,digits=4),formats[:p_fmt_center])
 
                 r += 5
